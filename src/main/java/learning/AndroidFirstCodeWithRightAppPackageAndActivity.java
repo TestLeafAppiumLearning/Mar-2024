3795bf76298108339eb4902e7c6b5ab73b685f43
Lokesh Kumar Thyagarajan <LokeshKumar.Thyagarajan@hidglobal.com>
2024-03-10 17:36:19 +0530

    Day 1 code added

--- src/main/java/learning/AndroidFirstCodeWithRightAppPackageAndActivity.java
+++ src/main/java/learning/AndroidFirstCodeWithRightAppPackageAndActivity.java
@@ -1,11 +1,13 @@
 package learning;
 
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.time.Duration;
 
 public class AndroidFirstCodeWithRightAppPackageAndActivity {
     public static void main(String[] args) throws URISyntaxException, MalformedURLException, InterruptedException {
@@ -17,6 +19,8 @@ public class AndroidFirstCodeWithRightAppPackageAndActivity {
         dc.setCapability("appActivity", "com.amazon.mShop.home.HomeActivity");
 //        dc.setCapability("deviceName","My emulator");
         AppiumDriver driver = new AppiumDriver(new URI("http://127.0.0.1:4723/").toURL(), dc);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.findElement(AppiumBy.id("com.amazon.mShop.android.shopping:id/sign_in_button")).click();
         Thread.sleep(5000);
         driver.quit();
     }
