6c64ad26efa1dd09bf05338eed60294c86b6b120
Lokesh Kumar Thyagarajan <LokeshKumar.Thyagarajan@hidglobal.com>
2024-03-10 17:31:47 +0530

    Day 1 code added

--- src/main/java/learning/AndroidFirstCodeUsingAppPackageAndAppActivity.java
+++ src/main/java/learning/AndroidFirstCodeUsingAppPackageAndAppActivity.java
@@ -1,7 +1,23 @@
 package learning;
 
-public class AndroidFirstCodeUsingAppPackageAndAppActivity {
-    public static void main(String[] args) {
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class AndroidFirstCodeUsingAppPackageAndAppActivity {
+    public static void main(String[] args) throws URISyntaxException, MalformedURLException, InterruptedException {
+        DesiredCapabilities dc = new DesiredCapabilities();
+        dc.setCapability("platformName", "Android");
+        dc.setCapability("automationName", "UiAutomator2");
+//        dc.setCapability("app",System.getProperty("user.dir")+"/apks/leaforg.apk");
+        dc.setCapability("appPackage", "com.testleaf.leaforg");
+        dc.setCapability("appActivity", "com.testleaf.leaforg.MainActivity");
+//        dc.setCapability("deviceName","My emulator");
+        AppiumDriver driver = new AppiumDriver(new URI("http://127.0.0.1:4723/").toURL(), dc);
+        Thread.sleep(5000);
+        driver.quit();
     }
 }
--- src/main/java/learning/AndroidFirstCodeWithRightAppPackageAndActivity.java
+++ src/main/java/learning/AndroidFirstCodeWithRightAppPackageAndActivity.java
@@ -1,7 +1,23 @@
 package learning;
 
-public class AndroidFirstCodeWithRightAppPackageAndActivity {
-    public static void main(String[] args) {
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class AndroidFirstCodeWithRightAppPackageAndActivity {
+    public static void main(String[] args) throws URISyntaxException, MalformedURLException, InterruptedException {
+        DesiredCapabilities dc = new DesiredCapabilities();
+        dc.setCapability("platformName", "Android");
+        dc.setCapability("automationName", "UiAutomator2");
+//        dc.setCapability("app",System.getProperty("user.dir")+"/apks/Amazon.apk");
+        dc.setCapability("appPackage", "com.amazon.mShop.android.shopping");
+        dc.setCapability("appActivity", "com.amazon.mShop.home.HomeActivity");
+//        dc.setCapability("deviceName","My emulator");
+        AppiumDriver driver = new AppiumDriver(new URI("http://127.0.0.1:4723/").toURL(), dc);
+        Thread.sleep(5000);
+        driver.quit();
     }
 }
